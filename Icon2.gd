0a7d6d67364c7d4bf9cd7f0c0313580698495341
100piecesT0ast <126206337+100piecesT0ast@users.noreply.github.com>
2023-03-10 11:53:24 -0800

    e

---
+++ Icon2.gd
@@ -0,0 +1,32 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+extends CharacterBody2D
+
+@export var speed = 400
+
+var target = position
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 10:
+		move_and_slide()
+		
