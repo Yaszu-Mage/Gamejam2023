08f3622eca7f23dfefefe51f3c9f028574a3a525
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2023-03-24 12:41:51 -0700

    PROCEED

--- assets/maps/movement.gd
+++ assets/maps/movement.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-onready var anim = get_node("AnimatedSprite")
+onready var anim = $AnimationPlayer
 var speed = 200  # speed in pixels/sec
 var velocity = Vector2.ZERO
 var upmove = false
@@ -8,54 +8,98 @@ var rightmove = false
 var leftmove = false
 var downmove = false
 var lastmove = null
+var idle = false
+var animfinished = false
+var collide = false
+
+
 
 func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
-		anim.play("rightwalk")
+		anim.play("idleright")
 		var rightmove = true
-		var lastmove = 1
+		lastmove = 1
 	else:
 		var rightmove = false
 	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
-		anim.play("leftwalk")
+		anim.play("idleleft")
 		var leftmove = true
-		var lastmove = 2
+		lastmove = 2
 	else:
 		var leftmove = false
 	if Input.is_action_pressed('ui_down'):
 		velocity.y += 1
-		anim.play("walkdown")
+		anim.play("idledown")
 		var downmove = true
-		var lastmove = 3
+		lastmove = 3
 	else:
 		var downmove = false
 	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
-		anim.play("upwalk")
+		anim.play("idleup")
 		var upmove = true
-		var lastmove = 4
+		lastmove == 4
 	else:
 		var upmove = false
 	if Input.is_action_just_released("ui_up"):
-		anim.play("upidle")
+		anim.play("idleup")
+		idle = true
+	else:
+		idle = false
 	if Input.is_action_just_released("ui_down"):
-		anim.play("idle")
+		anim.play("idledown")
+		idle = true
+	else:
+		idle = false
 	if Input.is_action_just_released("ui_right"):
-		anim.play("rightidle")
+		anim.play("idleright")
+		idle = true
+	else:
+		idle = false
 	if Input.is_action_just_released("ui_left"):
-		anim.play("leftidle")
-	
-	
-	# Make sure diagonal movement isn't faster
+		anim.play("idleleft")
+		idle = true
+	else:
+		idle = false
 	velocity = velocity.normalized() * speed
+func interact():
+		if Input.is_action_just_pressed("Interact"):
+			if lastmove == 1:
+				anim.play("leftint")
+			if lastmove == 3:
+				anim.play("upint")
+			if lastmove == 2:
+				anim.play("rightint")
+			if lastmove == 4:
+				anim.play("downint")
+
+func intanim():
+		if animfinished == true:
+			anim.stop()
+			if lastmove == 1:
+				anim.play("idleleft")
+			if lastmove == 3:
+				anim.play("idleup")
+			if lastmove == 2:
+				anim.play("idleright")
+			if lastmove == 4:
+				anim.play("idledown")
+
 	
 		
 		
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+	interact()
+	intanim()
 
 
+
+
+func _on_AnimationPlayer_animation_finished(upint):
+	animfinished = true
+	animfinished = false
