413273a6e8f8daca194208feda3d4c7aed13746f
Yaszu <sunguildstudios@gmail.com>
2023-04-25 07:14:59 -0700

    l

--- assets/maps/proceed.gd
+++ assets/maps/proceed.gd
@@ -2,9 +2,21 @@ extends Node2D
 onready var tilemap = $TileMap
 onready var bush = $TileMap2
 onready var trees = $Tilemap3
-var set_desert = {"29" : 5, "8" : 5, "7" : 5, "5" : 85, "31" : 5, "30" : 5, "62" : 5,}
-var set_plain = {"182" : 5, "186" : 5, "192" : 5, "174" : 85, "203" : 5, "212" : 5, "217" : 5, "224" : 5, "232" : 5, "241" : 5, "248" : 5, "256" : 5, "279" : 5, "310" : 5,}
-var set_ice = {"348" : 5, "364" : 5, "372" : 5, "377" : 5, "384" : 5, "391" : 5, "403" : 5, "406" : 5, "416" : 5, "422" : 5, "434" : 5, "448" : 5,}
+#Ghost was here
+#FUCK YOUUUUUUUUUUUUUUUUUUUUUUUUU
+#I know this shit because of modding on steam
+#Biome Documentation
+#Biome tilemaps/variables
+# 18 Ice Tiles/ 14 Spruce Tiles / 14 Plains Tiles / 6 fire tiles / 7 desert tiles
+#hey uh
+#biome list; plains, spruce, desert, ice, mesa, dark (noart), candy (noart), pirate (noart)
+#buddy, the ice is missing a few. ik you need to add them follow the same pattern also dont delete the comments we mush leave our self in historyyyyyyyyyyyyyyyyyyyyy ikr im so good at this
+#you aint doing to well at that.
+#imma try and figure out some meth shit
+var set_desert = {"desert" : 5, "desert1" : 5, "desert2" : 5, "desert3" : 5, "desert4" : 5, "desert5" : 5, "desert6" : 5}
+var set_plain = {"plains" : 5, "plains1" : 5, "plains2" : 5, "plains3" : 85, "plains4" : 5, "plains5" : 5, "plains6" : 5, "plains7" : 5, "plains8" : 5, "plains9" : 5, "plains10" : 5, "plains11" : 5, "plains12" : 5, "plains13" : 5,}
+var set_ice = {"ice" : 5, "ice1" : 5, "ice2" : 5, "ice3" : 5, "ice4" : 5, "ice5" : 5, "ice6" : 5, "ice7" : 5, "ice8" : 5, "ice9" : 5, "ice10" : 5, "ice11" : 5, "ice12" : 5, "ice13" : 5, "ice14" : 5, "ice15" : 5, "ice16" : 5, "ice17" : 5,}
+var set_fire = {"fire" : 5,}
 var desertnatrue = {"sprucestump" : 5, "sprucebush" : 5, "sprucebush2" : 5, "sprucehedge" : 5, "spruceleaf" : 5,}
 var deserttrees = {"" : 5,}
 var plainstrees = {"" : 5}
@@ -216,9 +228,32 @@ func ice():
 	else:
 		biome = randi() % 2 + 1
 		run = run + 1
-		if biome == 1:
-			make_chunk(16,16, set_desert)
-		if biome == 2:
-			make_chunk(16,16, set_ice)
+		biomes()
+	if run == 32:
+		desert = true
+
+func spruce():
+	hei = hei + 16
+	if biome >= 2:
+		make_chunk(16,16, set_desert)
+		make_plain_nature(16,16, desertnatrue)
+		run = run + 1
+	else:
+		biome = randi() % 2 + 1
+		run = run + 1
+		biomes()
+	if run == 32:
+		desert = true
+
+func fire():
+	hei = hei + 16
+	if biome >= 2:
+		make_chunk(16,16, set_fire)
+		make_plain_nature(16,16, firenature)
+		run = run + 1
+	else:
+		biome = randi() % 2 + 1
+		run = run + 1
+		biomes()
 	if run == 32:
 		desert = true
