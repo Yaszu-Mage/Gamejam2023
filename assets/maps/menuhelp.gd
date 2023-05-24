f170b942fc622db02e7e77a8ac91d08f4afab86e
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2023-05-24 14:56:02 -0700

    he;lp

---
+++ assets/maps/menuhelp.gd
@@ -0,0 +1,28 @@
+extends HBoxContainer
+@onready var invmenu = self
+var menvisible = false
+var color = null
+@onready var player = $"../.."
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_end"):
+		if is_multiplayer_authority():
+			if menvisible == true:
+				invmenu.visible = false
+				menvisible = false
+			else:
+				invmenu.visible = true
+				menvisible = true
+
+
+
+
+
+
+func _on_color_picker_color_changed(color):
+	player.self_modulate = color
