edb0ac17b3ea442f85ba0564fec66725a5d2b2fc
Yaszu <sunguildstudios@gmail.com>
2023-05-25 07:14:02 -0700

    Redid animations and Implemented Day/Night Cycle

--- assets/maps/CanvasModulate.gd
+++ assets/maps/CanvasModulate.gd
@@ -1,5 +1,17 @@
 extends CanvasModulate
-
+@onready var ani = $AnimationPlayer
 
 func _process(delta):
-	var time = 0
+	pass
+	
+func _ready():
+	var blood = randf_range(0.0, 100.0)
+	ani.play("Day_Night")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	var blood = randf_range(0.0, 100.0)
+	if blood <= 90:
+		ani.play("Day_Night")
+	else:
+		ani.play("Blood")
