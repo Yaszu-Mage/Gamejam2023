3e66b1c26f6ac1a1932edd3316febf2f4a1fe199
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2023-05-30 08:48:22 -0700

    some changes

---
+++ assets/maps/terraindetect.gd
@@ -0,0 +1,30 @@
+extends Area2D
+signal terraintype
+
+enum terrain {
+	plains = 0,
+	water = 1,
+	beach = 2,
+	jungle = 3,
+	jungle2 = 5,
+	desert = 4,
+	candy = 6,
+	fire = 7,
+	banana = 8,
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	_processtilemap(body, body_rid)
+
+func _processtilemap(body: Node2D, bodyrid: RID) -> void:
+	var currenttilemap = body
